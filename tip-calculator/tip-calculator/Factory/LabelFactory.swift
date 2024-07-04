9775ede28ba432ed4aa9936af0d966b9e8c1e90b
KaiFu <kevin12342247@gmail.com>
2024-07-05 02:41:26 +0800

    5. Add logo view details

---
+++ tip-calculator/tip-calculator/Factory/LabelFactory.swift
@@ -0,0 +1,23 @@
+/// <#Brief Description#> 
+///
+/// Created by TWINB00591630 on 2024/7/4.
+/// Copyright © 2024 Cathay United Bank. All rights reserved.
+
+import UIKit
+
+internal struct LabelFactory {
+    static func build(
+        text: String?,
+        font: UIFont,
+        backgroundColor: UIColor = .clear,
+        textColor: UIColor = ThemeColor.text,
+        textAlignment: NSTextAlignment = .center) -> UILabel {
+            let label = UILabel()
+            label.text = text
+            label.font = font
+            label.backgroundColor = backgroundColor
+            label.textColor = textColor
+            label.textAlignment = textAlignment 
+            return label
+    }
+}
--- tip-calculator/tip-calculator/View/LogoView.swift
+++ tip-calculator/tip-calculator/View/LogoView.swift
@@ -7,6 +7,50 @@ import UIKit
 
 class LogoView: UIView {
 
+    private let imageView: UIImageView = {
+        let view = UIImageView(image: .init(named: "icCalculatorBW"))
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+
+    private let topLabel: UILabel = {
+        let label = UILabel()
+        let text = NSMutableAttributedString(
+                string: "Mr TIP",
+                attributes: [.font: ThemeFont.demibold(ofSize: 16)])
+        text.addAttributes([.font: ThemeFont.bold (ofSize: 24)],range: NSMakeRange(3, 3))
+        label.attributedText = text
+        return label
+    }()
+
+    private let bottomLabel: UILabel = {
+        LabelFactory.build(text: "Calculator",
+                           font: ThemeFont.demibold(ofSize: 20),
+                           textAlignment: .left)
+
+    }()
+
+    private lazy var vStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            topLabel,
+            bottomLabel
+        ])
+        view.axis = .vertical
+        view.spacing = -4
+        return view
+    }()
+
+    private lazy var hStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            imageView,
+            vStackView
+        ])
+        view.axis = .horizontal
+        view.spacing = 8
+        view.alignment = .center
+        return view
+    }()
+
     init() {
         super.init(frame: .zero)
         layout()
@@ -17,7 +61,15 @@ class LogoView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .systemBlue
+        addSubview(hStackView)
+        hStackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+
+        imageView.snp.makeConstraints { make in
+            make.height.equalTo(imageView.snp.width)
+        }
     }
 
 }
