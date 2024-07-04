875691544a944dfb42ed633641a57f6be365afbb
KaiFu <kevin12342247@gmail.com>
2024-07-04 21:10:56 +0800

    Create: Project tip-calculator

---
+++ tip-calculator/tip-calculator/ViewController.swift
@@ -0,0 +1,17 @@
+/// <#Brief Description#> 
+///
+/// Created by TWINB00591630 on 2024/7/4.
+/// Copyright © 2024 Cathay United Bank. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tip-calculator/tip-calculatorUITests/tip_calculatorUITestsLaunchTests.swift
@@ -0,0 +1,30 @@
+/// <#Brief Description#> 
+///
+/// Created by TWINB00591630 on 2024/7/4.
+/// Copyright © 2024 Cathay United Bank. All rights reserved.
+
+import XCTest
+
+final class tip_calculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
