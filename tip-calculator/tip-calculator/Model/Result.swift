7f8ea5a6b3624e93dd3aeadb40814afd0c6a31cc
KaiFu <kevin12342247@gmail.com>
2024-07-08 00:32:58 +0800

    Setup ViewModel

---
+++ tip-calculator/tip-calculator/Model/Result.swift
@@ -0,0 +1,12 @@
+/// <#Brief Description#> 
+///
+/// Created by TWINB00591630 on 2024/7/8.
+/// Copyright © 2024 Cathay United Bank. All rights reserved.
+
+import Foundation
+
+struct Result {
+  let amountPerPerson: Double
+  let totalBill: Double
+  let totalTip: Double
+}
---
+++ tip-calculator/tip-calculator/service/AudioPlayerService.swift
@@ -0,0 +1,28 @@
+/// <#Brief Description#> 
+///
+/// Created by TWINB00591630 on 2024/7/8.
+/// Copyright © 2024 Cathay United Bank. All rights reserved.
+
+import Foundation
+import AVFoundation
+
+protocol AudioPlayerService {
+    func playSound()
+}
+
+
+final class DefaultAudioPlayer: AudioPlayerService {
+
+  private var player: AVAudioPlayer?
+
+  func playSound() {
+    let path = Bundle.main.path(forResource: "click", ofType: "m4a")!
+    let url = URL(fileURLWithPath: path)
+    do {
+      player = try AVAudioPlayer(contentsOf: url)
+      player?.play()
+    } catch(let error) {
+      print(error.localizedDescription)
+    }
+  }
+}
