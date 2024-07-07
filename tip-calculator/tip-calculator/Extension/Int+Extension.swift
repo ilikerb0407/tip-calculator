ed9fbd5b591dd46cf8dc9b71cc6dbcda92fe0461
KaiFu <kevin12342247@gmail.com>
2024-07-07 16:39:51 +0800

    11.  Add split input view details

---
+++ tip-calculator/tip-calculator/Extension/Int+Extension.swift
@@ -0,0 +1,10 @@
+/// <#Brief Description#>
+///
+/// Created by TWINB00591630 on 2024/7/7.
+/// Copyright © 2024 Cathay United Bank. All rights reserved.
+
+extension Int {
+    internal var stringValue: String {
+        String(self)
+    }
+}
--- tip-calculator/tip-calculator/View/SplitInputView.swift
+++ tip-calculator/tip-calculator/View/SplitInputView.swift
@@ -3,12 +3,75 @@
 /// Created by TWINB00591630 on 2024/7/4.
 /// Copyright © 2024 Cathay United Bank. All rights reserved.
 
+import Combine
+import CombineCocoa
 import UIKit
 
 class SplitInputView: UIView {
+    private let headerView: HeaderView = {
+        let view = HeaderView()
+        view.configure(topText: "Split", bottomText: "the total")
+        return view
+    }()
+
+    private lazy var decrementButton: UIButton = {
+        let button = buildButton(
+            text: "-",
+            corners: [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+        )
+        button.accessibilityIdentifier = ScreenIdentifier.SplitInputView.decrementButton.rawValue
+        button.tapPublisher.flatMap { [unowned self] _ in
+            Just(splitSubject.value == 1 ? 1 : splitSubject.value - 1)
+        }.assign(to: \.value, on: splitSubject)
+            .store(in: &cancellables)
+        return button
+    }()
+
+    private lazy var incrementButton: UIButton = {
+        let button = buildButton(
+            text: "+",
+            corners: [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        )
+        button.accessibilityIdentifier = ScreenIdentifier.SplitInputView.incrementButton.rawValue
+        button.tapPublisher.flatMap { [unowned self] _ in
+            Just(splitSubject.value + 1)
+        }.assign(to: \.value, on: splitSubject)
+            .store(in: &cancellables)
+        return button
+    }()
+
+    private lazy var quantityLabel: UILabel = {
+        let label = LabelFactory.build(
+            text: "1",
+            font: ThemeFont.bold(ofSize: 20),
+            backgroundColor: .white
+        )
+        label.accessibilityIdentifier = ScreenIdentifier.SplitInputView.quantityValueLabel.rawValue
+        return label
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            decrementButton,
+            quantityLabel,
+            incrementButton,
+        ])
+        stackView.axis = .horizontal
+        stackView.spacing = 0
+        return stackView
+    }()
+
+    private let splitSubject: CurrentValueSubject<Int, Never> = .init(1)
+    var valuePublisher: AnyPublisher<Int, Never> {
+        splitSubject.removeDuplicates().eraseToAnyPublisher()
+    }
+
+    private var cancellables = Set<AnyCancellable>()
+
     init() {
         super.init(frame: .zero)
         layout()
+        observe()
     }
 
     @available(*, unavailable)
@@ -16,7 +79,43 @@ class SplitInputView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func reset() {
+        splitSubject.send(1)
+    }
+
     private func layout() {
-        backgroundColor = .brown
+        [headerView, stackView].forEach(addSubview(_:))
+
+        stackView.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+        }
+
+        [incrementButton, decrementButton].forEach { button in
+            button.snp.makeConstraints { make in
+                make.width.equalTo(button.snp.height)
+            }
+        }
+
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalTo(stackView.snp.centerY)
+            make.trailing.equalTo(stackView.snp.leading).offset(-24)
+            make.width.equalTo(68)
+        }
+    }
+
+    private func observe() {
+        splitSubject.sink { [unowned self] quantity in
+            quantityLabel.text = quantity.stringValue
+        }.store(in: &cancellables)
+    }
+
+    private func buildButton(text: String, corners: CACornerMask) -> UIButton {
+        let button = UIButton()
+        button.setTitle(text, for: .normal)
+        button.titleLabel?.font = ThemeFont.bold(ofSize: 20)
+        button.backgroundColor = ThemeColor.primary
+        button.addRoundedCorners(corners: corners, radius: 8.0)
+        return button
     }
 }
